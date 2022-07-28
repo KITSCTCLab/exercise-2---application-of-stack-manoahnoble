2b1295c8c5f5aee19c505cf8be55fecad07ab1e1
manoahnoble <110010556+manoahnoble@users.noreply.github.com>
2022-07-28 21:34:41 +0530

    Third commit

--- Main.py
+++ Main.py
@@ -61,9 +61,9 @@ class Evaluate:
     #Returns:
     #    The result of evaluated postfix expression.
     self.stack = []
-#     for element in expression:
-#       if element.isdigit():
-#         self.push(int(element))
+    for element in expression:
+      if element.isdigit():
+        self.push(int(element))
       elif element in ["+", "-", "*", "/", "^"]:
         if element == "+":
           result = self.stack[-2] + self.stack[-1]
@@ -75,18 +75,18 @@ class Evaluate:
           result = self.stack[-2] // self.stack[-1]
         elif element == "^":
           result = self.stack[-2] ** self.stack[-1]
-#         self.pop()
-#         self.pop()
-#         self.push(result)
+        self.pop()
+        self.pop()
+        self.push(result)
     return self.pop()
     
 
 
 # Do not change the following code
 postfix_expression = input()  # Read postfix expression
-# tokens = postfix_expression.split()
-# evaluate = Evaluate(len(tokens))
-# if evaluate.validate_postfix_expression(tokens):
-#     print(evaluate.evaluate_postfix_expression(tokens))
-# else:
-#     print('Invalid postfix expression')
+tokens = postfix_expression.split()
+evaluate = Evaluate(len(tokens))
+if evaluate.validate_postfix_expression(tokens):
+    print(evaluate.evaluate_postfix_expression(tokens))
+else:
+    print('Invalid postfix expression')
